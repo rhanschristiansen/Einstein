ffa9cee937d43cf3a053b33559f4766e4a03ca9e
Robert Christiansen <rhanschristiansen@github.com>
2017-08-19 11:17:42 -0700

    added simulation data functionality to lidar_sensor.py

--- src/lidar/lidar_sensor.py
+++ src/lidar/lidar_sensor.py
@@ -1,11 +1,13 @@
 import sys
 import minimalmodbus
+import numpy as np
+import pandas as pd
 
 
 class LidarSensor(object):
-    def __init__(self):
-        self.mode = 'lidar'
+    def __init__(self, mode='lidar', simulation_data_file=None):
         try:
+            self.distance = 0
             self.instrument = minimalmodbus.Instrument('/dev/ttyUSB0', 1)  # port name, slave address (in decimal)
             self.instrument.serial.baudrate = 115200  # Baud
             self.instrument.serial.bytesize = 8
@@ -15,34 +17,64 @@ class LidarSensor(object):
             self.instrument.mode = minimalmodbus.MODE_RTU  # rtu or ascii mode
             # instrument.mode = minimalmodbus.MODE_ASCII   # rtu or ascii mode
             # instrument.debug = True
+            self.simulation_data = None
         except Exception as e:
             print 'ERROR OPENING MODBUS CONNECTION: {}\nWill continue using simulation data...'.format(sys.exc_info())
             self.mode = 'simulation'
+            if simulation_data_file is not None:
+                self.simulation_data = pd.read_csv(simulation_data_file)
 
         self.DISTANCE_1_REGISTER = 24
         self.CM_TO_FT = 0.0328084
 
-    def get_distance(self):
+    def get_distance(self, frame_number=None):
         distance = None
         if self.mode == 'lidar':
             try:
-                distance = self.instrument.read_register(self.DISTANCE_1_REGISTER, 1, functioncode=4) * self.CM_TO_FT
+                self.distance = self.instrument.read_register(self.DISTANCE_1_REGISTER, 1,
+                                                              functioncode=4) * self.CM_TO_FT
             except IOError:
                 # print("Failed to read from instrument at address {}".format(DISTANCE_1_REGISTER))
                 pass
             except ValueError:
                 # print("Failed to read from instrument at address {}".format(DISTANCE_1_REGISTER))
                 pass
-            return distance
+            return self.distance
         elif self.mode == 'simulation':
+            self.distance = self.get_simulation_data(frame_number)
+            return self.distance
+
+    def get_simulation_data(self, frame_number):
+
+        if self.simulation_data is None:
             return 12345
+        else:
+            # check whether frame number is out of bounds (no more sim data to be processed)
+            if frame_number > self.simulation_data['Frame'].values.max():
+                return -111
+            # get the corresponding lidar distance for the video frame
+            matching_dist = self.simulation_data.loc[
+                self.simulation_data['Frame'] == frame_number, 'Distance Lidar'].values
+
+            if len(matching_dist) == 0:
+                return self.distance  # return previous value #TODO: revisit this
+                # raise Exception('no data for frame {}!'.format(frame_number))
+            return matching_dist[0]
 
 
 def test():
-    lidar_sensor = LidarSensor()
+    lidar_sensor = LidarSensor(mode='simulation',
+                               simulation_data_file='/home/bob/PycharmProjects/Einstein/src/simulation_data/output43.csv')
+    frame_number = 7
     while True:
-        dist = lidar_sensor.get_distance()
+        dist = lidar_sensor.get_distance(frame_number=frame_number)
+        if dist == -111:
+            print 'simulation data finished.'
+            exit()
         if dist is not None:
             print dist
+        frame_number += 1
 
-# test()
+
+if __name__ == '__main__':
+    test()
