bc95ce69153f4309906f4fc5c93b10f483b64848
Bob <rhanschristiansen@gmail.com>
2018-06-16 11:05:05 -0700

    change list to string conversion for saving bounding boxes to csv

--- src/data_logging/data_logger.py
+++ src/data_logging/data_logger.py
@@ -1,9 +1,13 @@
 """
 A simple data logger
 """
+import os
 class DataLogger(object):
-    def __init__(self, filename, headers):
+    def __init__(self, filename, headers, overwrite=True):
         self.filename = filename
+        if overwrite is True:
+            if os.path.exists(self.filename):
+                os.remove(self.filename)
         self.headers = headers
         with open(self.filename, 'w') as f:
             self.__create_header(f)
@@ -23,6 +27,48 @@ class DataLogger(object):
                 for item in row:
                     f.write(str(item) + ',')
                 f.write('\n')
+
+    def log_bboxes(self, frame_num, bboxes):
+        """
+        Log the bbox detections for each frame.
+        There will be two columns: frame_num, bboxes.
+        
+        Notes:
+        bboxes will be in the following format:
+        each element of bb is space delimited, and each bb is underscore _ delimited
+        eg. '391 251 453 293_380 254 405 273_434 255 539 327'
+        
+        To parse this to list from csv, use:
+        
+            def parse_bbox(bb_str):
+                all_bboxes = []
+                for bb_s in bb_str.split('_'):
+                    vals = bb_s.split(' ')
+                    bbox_list = []
+                    for v in vals:
+                        bbox_list.append(int(v))
+                    all_bboxes.append(bbox_list)
+                return all_bboxes
+
+        """
+
+        def _list_2d_to_str(list_2d):
+            """
+            Convert from [[1,2,3],[4,5,6]] to '1 2 3_4 5 6'
+            """
+            output_str = ''
+            for idx2d, l in enumerate(list_2d):
+                for idx, val in enumerate(l):
+                    output_str += str(val)
+                    if idx < len(l) - 1:
+                        output_str += ' '
+
+                if idx2d < len(list_2d) - 1:
+                    output_str += '_'
+            return output_str
+        with open(self.filename, 'a') as f:
+            bbox_str = _list_2d_to_str(bboxes)
+            f.write(str(frame_num) + ',' + bbox_str + '\n')
 
 
 """
